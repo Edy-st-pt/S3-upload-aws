ef187b87af469034b2bf053713573bb40b60b814
Edson <edson_s_prestes@estudante.sc.senai.br>
2026-03-01 13:17:07 -0300

    Primeiro commit

---
+++ desafio/src/main/java/com/example/desafio/DesafioImplatancaoSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioImplatancaoSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioImplatancaoSistemaApplication.class, args);
+	}
+
+}
